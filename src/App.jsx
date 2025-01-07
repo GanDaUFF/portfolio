9f22e7562f79e89f7cbbaea34f1780ff127ea458
GandaUFF <qwert12121@hotmail.com>
2025-01-06 21:52:29 -0300

    refactor: ajustes em mostrar a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,34 @@ import NavBar from './Components/NavBar';
 
 export default function App() {
   const [hasScaled, setHasScaled] = useState(false);
+  const [showNavBar, setShowNavBar] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    // Primeiro timer para escalar a imagem
+    const scaleTimer = setTimeout(() => {
       setHasScaled(true);
-    }, 100); 
+    }, 100); // Delay inicial de 100ms
 
-    return () => clearTimeout(timer);
+    // Segundo timer para exibir a NavBar após a escala
+    const navBarTimer = setTimeout(() => {
+      setShowNavBar(true);
+    }, 1100); // 1000ms para a animação de escala + 100ms inicial
+
+    return () => {
+      clearTimeout(scaleTimer);
+      clearTimeout(navBarTimer);
+    };
   }, []);
 
   return (
     <div className="flex items-center bg-stone-900 justify-center w-screen h-screen">
-    <NavBar className={`${hasScaled ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`} />
-
+      <NavBar hasScaled={showNavBar} />
 
       <div
         className={`w-1/4 h-3/4 bg-stone-500 rounded-lg transform transition-transform duration-1000 ease-in-out ${
-          hasScaled ? 'scale-50' : 'scale-90'
+          hasScaled ? 'scale-75' : 'scale-100'
         }`}
       >
-        
         <img src={foto} alt="Minha imagem" className="w-full h-full object-cover rounded-lg" />
       </div>
     </div>
