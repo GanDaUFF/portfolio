4db7a2d22f66c0ce6169142b559969f89daac11a
GandaUFF <qwert12121@hotmail.com>
2025-01-09 22:15:28 -0300

    feat: adicionado os containers

--- src/App.jsx
+++ src/App.jsx
@@ -34,24 +34,34 @@ export default function App() {
   }, []);
 
   return (
-    <div className="bg-stone-900 w-screen h-screen flex flex-col">
+    <div className="bg-stone-900 w-screen h-screen flex flex-col items-center justify-center ">
       {/* NavBar */}
-      <NavBar hasScaled={showNavBar} />
+      <div className='flex w-[calc(100%-1.5rem)] h-16'><NavBar hasScaled={showNavBar} /></div>
 
       {/* Content Area */}
-      <div className="flex flex-row items-center justify-center flex-1 gap-8">
+      <div className="flex flex-row items-center  flex-1 ">
         {/* Image Container */}
-        <div
-          className={`w-1/4 h-3/4 bg-stone-500 rounded-lg transform transition-transform duration-1000 ease-in-out ${
-            hasScaled ? 'scale-75' : 'scale-100'
-          }`}
-          // ${hasMoved ? 'translate-x-40 -translate-y-12' : ''} // Movimento desativado
-        >
-          <img src={foto} alt="Minha imagem" className="w-full h-full object-cover rounded-lg" />
-        </div>
+        <div className="flex flex-col">
+          <div className="flex flex-row m-4 gap-4">
+          <div className="w-[600px] h-[350 px] bg-stone-700 rounded-lg shadow-lg flex items-center justify-center"></div>
+            <div
+              className={`w-[280px] h-[350 px] bg-stone-500 rounded-lg transform transition-transform duration-1000 ease-in-out ${hasScaled ? 'scale-100' : 'scale-110'
+                }`}
+            // ${hasMoved ? 'translate-x-40 -translate-y-12' : ''} // Movimento desativado
+            >
+              <img src={foto} alt="Minha imagem" className="w-full h-full object-cover rounded-lg" />
+            </div>
+          </div>
+          <div className="flex flex-row mx-4 gap-4">
 
+          <div className="w-[500px] h-[125px] bg-stone-700 rounded-lg shadow-lg"></div>
+          <div className="w-[380px] h-[125px] bg-stone-700 rounded-lg shadow-lg"></div>
+          </div>
+
+          
+        </div>
         {/* Placeholder for Additional Content */}
-        <div className="w-1/4 h-3/4 bg-stone-700 rounded-lg shadow-lg"></div>
+        <div className="w-[300px] h-[510px] bg-stone-700 rounded-lg shadow-lg"></div>
       </div>
     </div>
   );
